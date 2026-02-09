d60ba2b4ed0765cf71691da52ec9eb8c2140c09f
Link4Electronics <link4electronics@gmail.com>
2026-02-09 06:23:22 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -13,7 +13,11 @@ echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+if [ "${DEVEL_RELEASE-}" = 1 ]; then
+  make-aur-package taisei-git
+else
+  make-aur-package taisei
+fi
 
 # If the application needs to be manually built that has to be done down here
 
