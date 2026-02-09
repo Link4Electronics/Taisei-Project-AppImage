4d1eb9b81f0eab4a86ddd62a41520e8232461477
Link4Electronics <link4electronics@gmail.com>
2026-02-09 10:35:33 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -18,27 +18,30 @@ echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-make-aur-package cglm
+
 
 REPO="https://github.com/taisei-project/taisei"
 if [ "${DEVEL_RELEASE-}" = 1 ]; then
+  make-aur-package cglm
   make-aur-package taisei-git
-  echo "Making nightly build of Taisei-Project..."
-  echo "---------------------------------------------------------------"
-  VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
-  git clone --recursive --depth 1 "$REPO" ./taisei
+#  echo "Making nightly build of Taisei-Project..."
+#  echo "---------------------------------------------------------------"
+#  VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+#  git clone --recursive --depth 1 "$REPO" ./taisei
 else
-  echo "Making stable build of Taisei-Project..."
-  echo "---------------------------------------------------------------"
-  VERSION="$(git ls-remote --tags --sort="v:refname" "$REPO" | tail -n1 | sed 's/.*\///; s/\^{}//; s/^v//')"
-  git clone --branch v"$VERSION" --single-branch --recursive --depth 1 "$REPO" ./taisei
+make-aur-package cglm
+make-aur-package taisei
+#  echo "Making stable build of Taisei-Project..."
+#  echo "---------------------------------------------------------------"
+#  VERSION="$(git ls-remote --tags --sort="v:refname" "$REPO" | tail -n1 | sed 's/.*\///; s/\^{}//; s/^v//')"
+#  git clone --branch v"$VERSION" --single-branch --recursive --depth 1 "$REPO" ./taisei
 fi
-  echo "$VERSION" > ~/version
+#  echo "$VERSION" > ~/version
 
-  cd ./taisei
-  meson setup --prefix /usr --libexecdir lib --sbindir bin --buildtype plain --wrap-mode nodownload -D b_lto=true -D b_pie=true build
-  meson compile -C build
-  meson install -C build
+#  cd ./taisei
+#  meson setup --prefix /usr --libexecdir lib --sbindir bin --buildtype plain --wrap-mode nodownload -D b_lto=true -D b_pie=true build
+#  meson compile -C build
+#  meson install -C build
 
 # If the application needs to be manually built that has to be done down here
 
