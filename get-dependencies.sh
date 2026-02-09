107fbc251f3baabbeaf8c62bdc4e78446168d80e
Link4Electronics <link4electronics@gmail.com>
2026-02-09 07:22:10 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -19,11 +19,11 @@ get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
 if [ "${DEVEL_RELEASE-}" = 1 ]; then
-  make-aur-package taisei-git
+  make-aur-package cglm taisei-git
 else
-  make-aur-package taisei
+  make-aur-package cglm taisei
 fi
-make-aur-package cglm
+
 # If the application needs to be manually built that has to be done down here
 
 # if you also have to make nightly releases check for DEVEL_RELEASE = 1
